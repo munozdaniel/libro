24a1de40f0b498f828f0c841772de7a17db10bf1
munozdaniel <munozda87@hotmail.com>
2016-08-10 14:03:07 -0300

    setDatatables

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -21,4 +21,28 @@ class ControllerBase extends Controller
             )
         );
     }
+    protected function setDatatables()
+    {
+        $this->assets->collection('headerCss')
+            ->addCss('plugins/datatables/dataTables.bootstrap.css');
+        $this->assets->collection('footerJs')
+            ->addJs('plugins/datatables/jquery.dataTables.min.js')
+            ->addJs('plugins/datatables/dataTables.bootstrap.min.js');
+        $this->assets->collection('footerInlineJs')
+            ->addInlineJs('
+                        $(function () {
+                            var table = $(\'#tabla\').DataTable({
+                                paging: false,
+                                lengthChange: false,
+                                searching: false,
+                                ordering: true,
+                                info: true,
+                                autoWidth: true,
+                                scrollX: true,
+                                scrollCollapse: true,
+
+                            });
+                        });
+            ');
+    }
 }
