773a3bf4e5a4627c284f03153b374d24675024d4
munozdaniel <munozda87@hotmail.com>
2016-08-19 09:40:31 -0300

    SectoresController: CargarSectoresAjax

---
+++ app/controllers/SectoresController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmunioz
+ * Date: 19/08/2016
+ * Time: 9:04
+ */
+
+class SectoresController extends ControllerBase
+{
+    public function cargarSectoresAjaxAction()
+    {
+        $this->view->disable();
+        $retorno = array();
+        $phql  = "SELECT DISTINCT (otrodestino) as destino FROM Memo ORDER BY destino";
+        $sectores = $this->modelsManager->executeQuery($phql);
+        foreach ($sectores as $sector) {
+            $retorno[] = $sector->destino;
+        }
+        echo json_encode($retorno);
+        return;
+    }
+
+}
