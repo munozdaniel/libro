bcce3ed475171f63a4631e810e9521893b0ba195
munozdaniel <munozda87@hotmail.com>
2016-08-31 09:07:38 -0300

    Resoluciones Form

---
+++ app/forms/ResolucionesForm.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dmunioz
+ * Date: 31/08/2016
+ * Time: 8:57
+ */
+use Phalcon\Forms\Form;
+use Phalcon\Forms\Element\Text;
+use Phalcon\Forms\Element\TextArea;
+use Phalcon\Validation\Validator\PresenceOf;
+use \Phalcon\Forms\Element\Date;
+use Phalcon\Forms\Element\Hidden;
+use \Phalcon\Forms\Element\Select;
+
+class ResolucionesForm extends Form{
+    /**
+     * Initialize the products form
+     */
+    public function initialize($entity = null, $options = array())
+    {
+        $opcion['required'] = '';
+        $opcion['readOnly'] = '';
+        $opcion['asterisco'] = '';
+        $opcion['disabled'] = '';
+        if (isset($options['readOnly'])) {
+            $opcion['readOnly'] = 'readOnly';
+            $opcion['disabled'] = 'disabled';
+        }
+        if (isset($options['required'])) {
+            $opcion['required'] = 'required';
+            $opcion['asterisco'] = '<strong class="text-danger "> * </strong>';
+        }
+        /*========================== ID DOCUMENTO ==========================*/
+        if (!isset($options['edit'])) {
+            $elemento = new Text('id_documento',
+                array(
+                    'class' => 'form-control',
+                    'maxlength' => 70,
+                    $opcion['required'] => '',
+                    $opcion['readOnly'] => ''
+                ));
+            $elemento->setLabel(' ID');
+            $this->add($elemento);
+        } else {
+            $this->add(new Hidden("id_documento"));
+        }
+        /*========================== NRO ==========================*/
+        $elemento = new Text('nro_resolucion',
+            array(
+                'class' => 'form-control',
+                'placeholder' => 'Autogenerado',
+                'maxlength' => 70,
+                $opcion['required'] => '',
+                "readOnly" => 'true'
+            ));
+        $elemento->setLabel($opcion['asterisco'] . ' Nro de Nota');
+        $elemento->setFilters(array('int'));
+        $this->add($elemento);
+        /*========================== FECHA ==========================*/
+        $elemento = new Date('fecha',
+            array(
+                'class' => 'form-control',
+                'placeholder' => 'FECHA',
+                $opcion['required'] => '',
+                'readOnly' => ''
+            ));
+        $elemento->setLabel($opcion['asterisco'] . ' Fecha ');
+        $elemento->addValidators(array(
+            new PresenceOf(array(
+                'message' => 'La Fecha es requerida'
+            ))
+        ));
+        $this->add($elemento);
+        /*========================== SECTOR ORIGEN ==========================*/
+        $elemento = new Select('sector_id_oid', Sectores::find(array('sector_activo=1 AND sector_id!=1')), array(
+            'using' => array('sector_id', 'sector_nombre'),
+            'useEmpty' => true,
+            'emptyText' => 'Seleccionar ',
+            'emptyValue' => '',
+            'class' => 'form-control autocompletar',
+            $opcion['required'] => '',
+            $opcion['readOnly'] => '',
+            $opcion['disabled'] => '',
+            'style' => "width: 100%; height:40px !important;"
+        ));
+        $elemento->addValidators(array(
+            new PresenceOf(array(
+                'message' => 'Seleccione el sector de origen'
+            ))
+        ));
+        $elemento->setLabel($opcion['asterisco'] . 'Sector Origen');
+        $this->add($elemento);
+
+        /*========================== DESCRIPCION ==========================*/
+        $elemento = new TextArea('descripcion',
+            array(
+                'class' => 'form-control',
+                'placeholder' => 'Ingrese la descripción',
+                'maxlength' => 240,
+                $opcion['required'] => '',
+                $opcion['readOnly'] => ''
+            ));
+        $elemento->setLabel($opcion['asterisco'] . ' Descripción');
+        $elemento->setFilters(array('striptags', 'string'));
+        $elemento->addValidators(array(
+            new PresenceOf(array(
+                'message' => 'La descripción es requerida.'
+            ))
+        ));
+        $this->add($elemento);
+        /*========================== CREADO POR ==========================*/
+
+        $elemento = new Text('creadopor',
+            array(
+                'class' => 'form-control',
+                'placeholder' => 'Creado Por',
+                'maxlength' => 70,
+                $opcion['required'] => '',
+                "readOnly" => 'true'
+            ));
+        $elemento->setLabel($opcion['asterisco'] . ' Creado Por');
+        $miSesion = $this->session->get('auth');
+        $elemento->setDefault($miSesion['usuario_nombreCompleto']);
+        $elemento->setFilters(array('striptags', 'string'));
+        $this->add($elemento);
+    }
+}
