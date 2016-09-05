867b159a79f2196a6b34effa199f077a2d7608d0
munozdaniel <munozda87@hotmail.com>
2016-09-05 09:52:24 -0300

    Usuarios.ver usuarios

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -32,5 +32,29 @@ class UsuariosController extends ControllerBase
 
     }
 
+    public function verUsuariosAction()
+    {
+        $this->view->pick('usuarios/verUsuariosPorRol');
+        $this->setDatatables();
+
+        $numberPage = $this->request->getQuery("page", "int");
+        $parameters["order"] = "usuario_id DESC";
+
+        $documento = Usuarioporrol::find($parameters);
+
+        if (count($documento) == 0) {
+            $this->flashSession->warning("<i class='fa fa-warning'></i> No se encontraron usuarios cargados");
+        }
+
+        $paginator = new Paginator(array(
+            "data" => $documento,
+            "limit" => 10,
+            "page" => $numberPage
+        ));
+
+        $this->view->page = $paginator->getPaginate();
+
+    }
+
 
 }
