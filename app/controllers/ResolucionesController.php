d0c7701b25bf954bb8366859d56efa7dd57e0df7
Muñoz Daniel <dmunioz@imps.org.ar>
2016-09-29 11:26:33 -0300

    Resoluciones: Busqueda completa con criteria

--- app/controllers/ResolucionesController.php
+++ app/controllers/ResolucionesController.php
@@ -418,7 +418,39 @@ class ResolucionesController extends ControllerBase
         $this->setDatatables();
         $numberPage = 1;
         if ($this->request->isPost()) {
+            $_POST['descripcion'] = strtoupper($_POST['descripcion']);
+            if ($this->request->getPost('nro_resolucion_final', 'int') == null) {
+                $_POST['nro_resolucion']=$this->request->getPost('nro_resolucion_inicial', 'int');
+            }
+            if ($this->request->getPost('fecha_final') == null) {
+                $_POST['fecha']=$this->request->getPost('fecha_inicial');
+            }
+
             $query = Criteria::fromInput($this->di, "Resoluciones", $_POST);
+
+            if ($this->request->getPost('nro_resolucion_final', 'int') != null
+                && $this->request->getPost('nro_resolucion_inicial', 'int') != null) {
+                $query->andWhere('nro_resolucion BETWEEN '. $this->request->getPost('nro_resolucion_inicial', 'int') ." AND ". $this->request->getPost('nro_resolucion_final', 'int'));
+            }
+            if ($this->request->getPost('fecha_final') != null) {
+                if($this->request->getPost('fecha_inicial') != null) {
+                    $query->betweenWhere('fecha', $this->request->getPost('fecha_inicial'), $this->request->getPost('fecha_final'));
+                }
+                else
+                {
+                    $this->flashSession->error("Verifique que la fecha inicial y final estén correctamente ingresadas");
+                    return $this->response->redirect('resoluciones/index');
+                }
+            }
+            $rol_id = $this->session->get('auth')['rol_id'];
+            if($rol_id != 2 )//2:Administrador
+            {
+                //Los que no son ADMIN solo pueden ver los doc del ultimo año. Y los habilitados
+                $date = date_create(date('Y') . '-01-01');
+                $ultimoAnio = date_format($date, "Y-m-d");//A pedido. los usuarios normales solo podrán ver las expedientes del ultimo año.
+                $query->andWhere(" '$ultimoAnio' <= fecha AND habilitado=1 ");
+            }
+            //var_dump($query->getParams());
             $this->persistent->parameters = $query->getParams();
         } else {
             $numberPage = $this->request->getQuery("page", "int");
@@ -428,19 +460,7 @@ class ResolucionesController extends ControllerBase
         if (!is_array($parameters)) {
             $parameters = array();
         }
-        $rol = $this->session->get('auth')['rol_nombre'];
-        $limitarAnio = "";
-        if ($rol != "ADMINISTRADOR") {
-            $date = date_create(date('Y') . '-01-01');
-            $ultimoAno = date_format($date, "Y-m-d");//A pedido. los usuarios normales solo podrán ver las resoluciones del ultimo año.
-            $limitarAnio = "  '$ultimoAno'  <= fecha ";
-        }
-        if (isset($parameters['conditions']))
-            if ($limitarAnio != "")
-                $parameters['conditions'] .= "AND $limitarAnio ";
-            else
-                if ($limitarAnio != "")
-                    $parameters['conditions'] = "$limitarAnio ";
+
 
         $parameters["order"] = "id_documento DESC";
 
@@ -459,105 +479,6 @@ class ResolucionesController extends ControllerBase
         $this->view->page = $paginator->getPaginate();
     }
 
-    public function searchEntreFechasAction()
-    {
-        $this->setDatatables();
-        $this->view->pick('resoluciones/search');
-        $numberPage = 1;
-
-        $rol = $this->session->get('auth')['rol_nombre'];
-        $limitarAnio = "";
-        if ($rol != "ADMINISTRADOR") {
-            $date = date_create(date('Y') . '-01-01');
-            $ultimoAno = date_format($date, "Y-m-d");//A pedido. los usuarios normales solo podrán ver las resoluciones del ultimo año.
-            $limitarAnio = "  '$ultimoAno'  <= fecha AND ";
-        }
-        $desde = $this->request->getPost('fecha_desde');
-        $hasta = $this->request->getPost('fecha_hasta');
-        if ($this->request->isPost()) {
-
-            $query = Criteria::fromInput($this->di, "Resoluciones", $_POST);
-            $this->persistent->parameters = $query->getParams();
-        } else {
-            $numberPage = $this->request->getQuery("page", "int");
-        }
-
-        $parameters = $this->persistent->parameters;
-        if (!is_array($parameters)) {
-            $parameters = array();
-        }
-        $parameters["order"] = "id_documento DESC";
-        if ($desde != null && $hasta != null) {
-            if (isset($parameters['conditions']))
-                $parameters['conditions'] .= " AND $limitarAnio  fecha BETWEEN '$desde' AND '$hasta'";
-            else
-                $parameters['conditions'] = "$limitarAnio  fecha BETWEEN '$desde' AND '$hasta'";
-        }
-        $resoluciones = Resoluciones::find($parameters);
-        if (count($resoluciones) == 0) {
-            $this->flashSession->warning("<i class='fa fa-warning'></i> No se encontraron resoluciones cargadas en el sistema que coincidan con su búsqueda");
-            return $this->response->redirect('resoluciones/index');
-        }
-
-        $paginator = new Paginator(array(
-            "data" => $resoluciones,
-            "limit" => 10,
-            "page" => $numberPage
-        ));
-
-        $this->view->page = $paginator->getPaginate();
-    }
-
-
-    public function searchEntreNumerosAction()
-    {
-        $this->setDatatables();
-        $this->view->pick('resoluciones/search');
-        $numberPage = 1;
-
-        $rol = $this->session->get('auth')['rol_nombre'];
-        $limitarAnio = "";
-        if ($rol != "ADMINISTRADOR") {
-            $date = date_create(date('Y') . '-01-01');
-            $ultimoAno = date_format($date, "Y-m-d");//A pedido. los usuarios normales solo podrán ver las resoluciones del ultimo año.
-            $limitarAnio = " '$ultimoAno'  <= fecha AND ";
-        }
-        $desde = $this->request->getPost('nroInicial');
-        $hasta = $this->request->getPost('nroFinal');
-        if ($this->request->isPost()) {
-
-            $query = Criteria::fromInput($this->di, "Resoluciones", $_POST);
-            $this->persistent->parameters = $query->getParams();
-        } else {
-            $numberPage = $this->request->getQuery("page", "int");
-        }
-
-        $parameters = $this->persistent->parameters;
-        if (!is_array($parameters)) {
-            $parameters = array();
-        }
-        $parameters["order"] = "id_documento DESC";
-
-        if ($desde != null && $hasta != null) {
-            if (isset($parameters['conditions']))
-                $parameters['conditions'] .= " AND $limitarAnio  nro_resolucion >= $desde AND nro_resolucion <= $hasta";
-            else
-                $parameters['conditions'] = "$limitarAnio  nro_resolucion >= $desde AND nro_resolucion <= $hasta";
-        }
-        $resoluciones = Resoluciones::find($parameters);
-        if (count($resoluciones) == 0) {
-            $this->flashSession->warning("<i class='fa fa-warning'></i> No se encontraron resoluciones cargadas en el sistema que coincidan con su búsqueda");
-            return $this->response->redirect('resoluciones/index');
-        }
-
-        $paginator = new Paginator(array(
-            "data" => $resoluciones,
-            "limit" => 10,
-            "page" => $numberPage
-        ));
-
-        $this->view->page = $paginator->getPaginate();
-    }
     /**
      * Listar Las Resoluciones
      */
